cf4588a3456200ea7f71a76e76ab6f35657a72ea
carolseiffarth <78746548+carolseiffarth@users.noreply.github.com>
2022-03-23 21:59:18 -0300

    Create codigo.ino

---
+++ ex3/codigo.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(4, OUTPUT);
+    
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(12, HIGH);
+  delay(2000);
+  digitalWrite(8, HIGH);
+  delay(2000);
+  digitalWrite(7, HIGH);
+  delay(2000);
+  digitalWrite(4, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(4, LOW);
+  delay(2000);
+}
